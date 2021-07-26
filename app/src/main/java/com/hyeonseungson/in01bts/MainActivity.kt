daaa3c0a3c1ec6a0a9d269c047199882e467e3f5
HyeonseungSon <boywin1992@naver.com>
2021-07-26 14:39:59 +0900

    ManDeum0726

--- app/src/main/java/com/hyeonseungson/in01bts/MainActivity.kt
+++ app/src/main/java/com/hyeonseungson/in01bts/MainActivity.kt
@@ -2,10 +2,27 @@ package com.hyeonseungson.in01bts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1. 사진 선택
+        val image1 = findViewById<ImageView>(R.id.image_1)
+        val image2 = findViewById<ImageView>(R.id.image_2)
+        val image3 = findViewById<ImageView>(R.id.image_3)
+        val image4 = findViewById<ImageView>(R.id.image_4)
+        val image5 = findViewById<ImageView>(R.id.image_5)
+        val image6 = findViewById<ImageView>(R.id.image_6)
+        val image7 = findViewById<ImageView>(R.id.image_7)
+
+        image1.setOnClickListener {
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show()
+        }
+
+        // 2. 해당 사진 출력
     }
 }
