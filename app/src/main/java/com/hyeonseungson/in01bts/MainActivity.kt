4d593145856957d8eed229bbb917a99e851df334
HyeonseungSon <boywin1992@naver.com>
2021-07-26 15:26:56 +0900

    ManDeum0726

--- app/src/main/java/com/hyeonseungson/in01bts/MainActivity.kt
+++ app/src/main/java/com/hyeonseungson/in01bts/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hyeonseungson.in01bts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 1. 사진 선택
+        // 1. BTS 사진 선택
         val image1 = findViewById<ImageView>(R.id.image_1)
         val image2 = findViewById<ImageView>(R.id.image_2)
         val image3 = findViewById<ImageView>(R.id.image_3)
@@ -20,9 +21,41 @@ class MainActivity : AppCompatActivity() {
         val image7 = findViewById<ImageView>(R.id.image_7)
 
         image1.setOnClickListener {
-            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "1번 클릭 완료\ntest06", Toast.LENGTH_LONG).show()
+
+            // 2. 해당 사진 전체화면 출력
+            val intent = Intent(this, BTS1Activity::class.java)
+            startActivity(intent)
         }
 
-        // 2. 해당 사진 출력
+        image2.setOnClickListener {
+            val intent = Intent(this, BTS2Activity::class.java)
+            startActivity(intent)
+        }
+
+        image3.setOnClickListener {
+            val intent = Intent(this, BTS3Activity::class.java)
+            startActivity(intent)
+        }
+
+        image4.setOnClickListener {
+            val intent = Intent(this, BTS4Activity::class.java)
+            startActivity(intent)
+        }
+
+        image5.setOnClickListener {
+            val intent = Intent(this, BTS5Activity::class.java)
+            startActivity(intent)
+        }
+
+        image6.setOnClickListener {
+            val intent = Intent(this, BTS6Activity::class.java)
+            startActivity(intent)
+        }
+
+        image7.setOnClickListener {
+            val intent = Intent(this, BTS7Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
